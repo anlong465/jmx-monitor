1071fd33e4566eb1c239ab622596a2b72b19106f
yonghong.chen <13585731329@139.com>
2022-05-20 14:21:22 +0800

    Add JMX for DirectMemory

--- devops/build.sh
+++ devops/build.sh
@@ -1,5 +1,5 @@
 rm -f *.jar
-cp ../jmx-agent/target/*.jar .
+cp ../jmx-agent-assembly/target/jmx-agent-1.0.jar .
 cp ../jmx-exporter/target/*.jar .
 podman build . -t registry.sunrise.com:5000/jmx/jmx-exporter:$1
 podman push registry.sunrise.com:5000/jmx/jmx-exporter:$1
